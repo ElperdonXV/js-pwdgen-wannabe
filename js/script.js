aca41a56feb802dfe5e2a23a7ea1019d04d36562
ElperdonXV <davidemartinografico@gmail.com>
2021-11-18 14:01:41 +0100

    all done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
     // Descrizione esercizio:
     // Chiedi all’utente il suo nome,
     // poi chiedi il suo cognome,
-    //    poi chiedi il suo colore preferito
+    // poi chiedi il suo colore preferito
     // Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+    let nome = prompt('Inserisci Nome');
+    console.log(nome);
+
+    let cognome = prompt('Inserisci Cognome');
+    console.log(cognome);
+
+    let colorePref = prompt('Inserisci il tuo colore preferito');
+    console.log(colorePref);
+    let password = `
+       ${nome}${cognome}${colorePref}21
+     `;
+    console.log(password);
+    document.getElementById('password').innerHTML = password;
